9285377d4e9763ed8da679602608484308b5b5b6
Joshua Adebayo <95285179+JoshuaAdebayo@users.noreply.github.com>
2021-12-17 16:57:01 +0000

    Update covid_news_testing.py

--- flaskr/covid_news_testing.py
+++ flaskr/covid_news_testing.py
@@ -2,21 +2,22 @@
 In this module I test functions within covid_news_handling.py
 """
 import sched
+import time
 
-from flaskr.covid_news_handling import news_API_request
-from flaskr.covid_news_handling import schedule_news_updates
+from covid_news_handling import news_API_request
+from covid_news_handling import schedule_news_updates
 
-def news_API_testing():
+def test_news_API_requests():
 	news = news_API_request("Covid Covid-19 coronavirus")
 	assert len(news) > 1
     
- def test_schedule_news_updates():
-    s = sched.scheduler(time.time, time.sleep)
-    assert len(s.queue) == 0 
-    schedule_news_updates(5, "test")
-    assert len(s.queue) == 1
-    s.run(blocking = False)
-    assert len(s.queue) == 1
-    time.sleep(3)
-    assert len(s.queue) == 0 
+def test_schedule_news_updates():
+        s = sched.scheduler(time.time, time.sleep)
+        assert len(s.queue) == 0 
+        schedule_news_updates(5, "test")
+        assert len(s.queue) == 1
+        s.run(blocking = False)
+        assert len(s.queue) == 1
+        time.sleep(5)
+        assert len(s.queue) == 0 
  
